1d9879259b51a14fbb810d4a6cd39768ae3ac39d
raynoe <raynoe_lxd@hotmail.com>
2013-06-17 21:15:45 +0700

    devise login

--- app/models/thread_forum.rb
+++ app/models/thread_forum.rb
@@ -1,4 +1,5 @@
 class ThreadForum < ActiveRecord::Base
-  # before_filter :authenticate_user!,:except => [:index, :show]
+  #before_filter :authenticate_user!,:except => [:index, :show]
   attr_accessible :body, :category_id, :is_deleted, :reputation_point, :status, :title, :user_id
 end
+
