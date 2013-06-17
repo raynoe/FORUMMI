d28708a52ffd3ef47a0b69595f2f0639c5d315f9
raynoe <raynoe_lxd@hotmail.com>
2013-06-17 22:42:44 +0700

    fix thread error

--- app/models/thread_forum.rb
+++ app/models/thread_forum.rb
@@ -1,5 +1,9 @@
 class ThreadForum < ActiveRecord::Base
   #before_filter :authenticate_user!,:except => [:index, :show]
   attr_accessible :body, :category_id, :is_deleted, :reputation_point, :status, :title, :user_id
+
+  belongs_to :user
+
+  validates :title, :body, :presence => true
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  has_many :thread_forum
 end
