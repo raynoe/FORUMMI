f67726ac4b59dd0136f999f3e3ea58240d3a030f
rfauzi <br3hom@yahoo.co.id>
2013-06-22 15:07:35 +0700

    DONT REPEAT YOURSELF

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "UKM" )
+Category.create(name: "Event" )
+Category.create(name: "Perkuliahan" )
+
